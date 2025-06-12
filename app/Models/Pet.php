78ead6e7d9edb68ad4f342e984cfb5eba0cf4009
WilghanerSilva <wilghener@gmail.com>
2025-06-11 23:57:59 -0300

    Criação do model Pet

--- app/Models/Pet.php
+++ app/Models/Pet.php
@@ -19,4 +19,9 @@ class Pet extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function services()
+    {
+        return $this->hasMany(Service::class);
+    }
 }
