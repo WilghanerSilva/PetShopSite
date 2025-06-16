c4bb239dc436f040643e9a3d979298f947ef257c
WilghanerSilva <wilghener@gmail.com>
2025-06-15 21:04:09 -0300

    Refatoração da nomeclatura de arquivos

--- resources/js/components/service-card.tsx
+++ resources/js/components/service-card.tsx
@@ -4,7 +4,7 @@ interface CardProps {
     service: Service
 }
 
-export default function ServiceCard(props:CardProps) {
+export default function ServiceCard({service}:CardProps) {
 
     const dateFormat = (date:string) => {
         const parts = date.split("-");
@@ -17,11 +17,11 @@ export default function ServiceCard(props:CardProps) {
         <div className="w-md border border-(--orange) rounded-xl pl-4 shadow-(--shadow) bg-(--orange) hover:cursor-pointer">
             <div className="flex justify-between items-center bg-(--primary-white) rounded-r-xl p-2">
                 <div className="flex flex-col gap-2">
-                    <h1 className="font-bold text-xl">{props.service.pet_name}</h1>
-                    <p className="font-semibold">Serviço: {props.service.type}</p>
-                    <p>Data: {dateFormat(props.service.data)}</p>
+                    <h1 className="font-bold text-xl">{service.pet_name}</h1>
+                    <p className="font-semibold">Serviço: {service.type}</p>
+                    <p>Data: {dateFormat(service.date)}</p>
                 </div>
-                <p className="font-semibold rounded-2xl bg-(--light-yellow) px-4 py-1">{props.service.is_done ? "Finalizado" : "Marcado"}</p>
+                <p className="font-semibold rounded-2xl bg-(--light-yellow) px-4 py-1">{service.is_done ? "Finalizado" : "Marcado"}</p>
             </div>
         </div>
     )
