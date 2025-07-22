5d93df8e079a813a53840d40dd610b3cbc2e94c4
WilghanerSilva <wilghener@gmail.com>
2025-07-22 11:16:37 -0300

    Refatoração do resolve do vite

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -8,10 +8,12 @@ import { HelmetProvider } from "react-helmet-async";
 import { ThemeProvider } from "./context/ThemeContext";
 import { PosSessionProvider } from "./context/PosSessionContext";
 
-
+const pages = import.meta.glob('./pages/**/*.tsx');
 
 createInertiaApp({
-  resolve: name => import(`./pages/${name}`).then(module => module.default),
+  resolve: name =>
+  (pages[`./pages/${name}.tsx`] as () => Promise<{ default: React.ComponentType }> )()
+    .then(module => module.default),
   setup({ el, App, props }) {
     createRoot(el).render(
       <React.StrictMode>
