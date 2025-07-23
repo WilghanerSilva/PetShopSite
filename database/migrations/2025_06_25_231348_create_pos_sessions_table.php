60560bd193945d35c8d3b94b3479cf5ea4984f93
WilghanerSilva <wilghener@gmail.com>
2025-07-23 16:14:21 -0300

    Correção de outra migration

--- database/migrations/2025_06_25_231348_create_pos_sessions_table.php
+++ database/migrations/2025_06_25_231348_create_pos_sessions_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('pos_sessions', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->foreignId('user_id')->constrained('user')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
         });
     }
 
